e3575cd628b1ff7c0b3161513a4c94a622d6f2bc
Erick Lui <erickjin.lui@gmail.com>
2017-06-16 00:48:52 -0700

    displayed object data on html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,33 @@ import { HttpService } from './http.service';
 })
 export class AppComponent {
   weather: any = '';
-  constructor(private _httpService: HttpService){}
+  humidity: number;
+  temp_avg: number;
+  temp_high: number;
+  temp_low: number;
+  status: '';
+  constructor(private _httpService: HttpService){
+    this._httpService.retrieveWeather('san jose')
+    .then( weather => {
+      this.weather = weather;
+      this.humidity = weather.main.humidity;
+      this.temp_avg = Math.round((9/5)*(weather.main.temp-273.15)+32);
+      this.temp_high = Math.round((9/5)*(weather.main.temp_max-273.15)+32);
+      this.temp_low = Math.round((9/5)*(weather.main.temp_min-273.15)+32);
+      this.status = weather.weather[0].description;
+    })
+    .catch( err => { console.log(err);})
+  }
   getWeather(city){
-    this.weather = this._httpService.retrieveWeather(city);
-    console.log(this.weather);
-    
-    
+    this._httpService.retrieveWeather(city)
+    .then( weather => {
+      this.weather = weather;
+      this.humidity = weather.main.humidity;
+      this.temp_avg = Math.round((9/5)*(weather.main.temp-273.15)+32);
+      this.temp_high = Math.round((9/5)*(weather.main.temp_max-273.15)+32);
+      this.temp_low = Math.round((9/5)*(weather.main.temp_min-273.15)+32);
+      this.status = weather.weather[0].description;
+    })
+    .catch( err => { console.log(err);})
   }
 }
